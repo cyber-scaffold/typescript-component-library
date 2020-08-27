e7b50611305471d375c50f00bed80977fe8a57f7
shicheng.yu <yushicheng@hztianque.com>
2020-08-27 09:40:49 +0800

    2020年08月27日 09:48的快速提交

--- scripts/development.js
+++ scripts/development.js
@@ -1,21 +1,17 @@
 const path = require("path");
-const watch = require("watch");
-const execa = require("execa");
-// const concurrently = require("concurrently");
+const concurrently = require("concurrently");
 
-const watchPath = path.resolve(__dirname, "../src/");
 
-async function developmentTask() {
+(async function developmentTask() {
   try {
-    await execa("npx", ["cyber-component", "build"], { stdout: "inherit" });
-    await execa("npx", ["cyber-spa", "dev"], { stdout: "inherit" });
+    await concurrently([
+      { name: "转换组件代码", command: "npx cyber-component dev" },
+      { name: "webpack调试", command: "npx cyber-spa dev" }
+    ]);
   } catch (error) {
     throw error;
   } finally {
     process.exit(0);
   };
-};
-
-watch.watchTree(watchPath, developmentTask);
-
+})();
 
